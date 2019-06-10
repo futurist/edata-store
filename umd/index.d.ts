af997edec7e3ca76438b7148d1ee206d78586bd7
jamesyang.yjm <jamesyang.yjm@alibaba-inc.com>
2019-06-10 19:15:16 +0800

    namespace arg optional

--- umd/index.d.ts
+++ umd/index.d.ts
@@ -10,11 +10,11 @@ export declare const defaultCache: any;
  * @param cache {Object} The global cache store
  * @returns {edataRoot} The edata root instance
  */
-export declare function getStore(namespace: string, initStore?: (e: any) => edataRoot, cache?: any): edataRoot;
+export declare function getStore(namespace?: string, initStore?: (e: any) => edataRoot, cache?: any): edataRoot;
 /**
  * Get 2 levels store from global cache store
  * @param namespace1 {string} The level1 namespace of global cache store, namespace2 is level1
  * @param cache {Object} The global cache store
  * @returns {Function} (namespace2: string, initStore = (e => edata({}))) => edataRoot
  */
-export declare function getStore2(namespace1: string, cache?: any): (namespace2: string, initStore?: (e: any) => edataRoot) => edataRoot;
+export declare function getStore2(namespace1?: string, cache?: any): (namespace2?: string, initStore?: (e: any) => edataRoot) => edataRoot;
